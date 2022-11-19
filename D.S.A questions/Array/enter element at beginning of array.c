ece0fe490ed409add82cc1a3ed8ce9ffab9016d6
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2022-11-19 17:43:53 +0530

    Update enter element at beginning of array.c

--- D.S.A questions/Array/enter element at beginning of array.c
+++ D.S.A questions/Array/enter element at beginning of array.c
@@ -25,4 +25,24 @@ int main(){
     }
 }
 --------------------------------------------------------------------------------------------------------------------------------
-
+// 4) This one will do trio work insertion at beginnig,end,given by user. Just change p value to change index value and data value to change index value.
+  
+#include <stdio.h>
+int main(){
+    int arr[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = 10, i, p=0,data=100;
+    printf("before array element--\n");
+    for (i = 0; i < n; i++){
+        printf("%d\t", arr[i]);
+    }
+    for (i = n; i >= data; i--){
+        arr[i + 1] = arr[i];
+    }
+    arr[p] = data;
+    n++;
+    printf("\nafter insertion array element are-- \n");
+    for (i = 0; i < n; i++){
+        printf("%d\t", arr[i]); 
+    }
+}
+-----------------------------------------------------------------------------------------------------------------------------------------
