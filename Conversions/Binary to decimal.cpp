927b8603730ecf2b1472935dc0177ae2bfb775f3
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-01-16 07:43:34 +0530

    binary

---
+++ Conversions/Binary to decimal.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int dec = 0, i = 0, rem,n;
+    cout << "Enter a number: "; 
+    cin >> n;
+
+  while (n!=0)
+   {
+    rem = n % 10;
+    n /= 10;
+    dec = dec + rem * pow(2, i);
+    ++i;
+  }
+  cout << dec;
+    
+}
