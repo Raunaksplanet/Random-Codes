81a9d5576c2fba9143c2752507af1ae3891be530
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-01-16 15:48:48 +0530

    Create Binary to decimal using function in.cpp

---
+++ Functions/Binary to decimal using function in.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int BtoD(int n)
+{
+	int dec = 0, i = 0, rem;
+	while (n!=0)
+   {
+    rem = n % 10;
+    n /= 10;
+    dec = dec + rem * pow(2, i);
+    ++i;
+  }
+  return dec;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a binary number: "; 
+    cin >> n;
+
+  cout << BtoD(n);
+    
+}
