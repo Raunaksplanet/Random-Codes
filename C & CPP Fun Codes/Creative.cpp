4c48b3cb0a60f774fb9668b3ddbb0c7d0dafbb34
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-08-30 11:53:35 +0530

    Update Creative.cpp

--- C & CPP Fun Codes/Creative.cpp
+++ C & CPP Fun Codes/Creative.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+
 int main()
 {
-    string str = "ronak";
+    string str = "raunaks";
     // cout << "Enter String: ";
     // cin >> str;
-    int c, b, a = str.length();
-    c = b = a/2;
-    if(a % 2 != 0)
+
+    int c = 1, b, a = str.length();
+    if (a % 2 != 0)
     {
+        b = a / 2;
         cout << str[b] << endl;
-        for(int i = 0; i <= 4; i++)
+        for (int i = 1,j=2,c = 1; i <= a / 2 + 1; i++,++j)
         {
-            cout <<  str.substr(--b,++c) << endl;
+            
+            cout << setw() << str.substr(--b, ++j) << endl;
         }
     }
 }
