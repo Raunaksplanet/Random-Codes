9bece599a0da708acd8ddde822d1d9d0af2dc016
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-09-17 19:51:08 +0530

    Update Swing_Module.java

--- Mini Projects/Java/Swing_Module.java
+++ Mini Projects/Java/Swing_Module.java
@@ -1,1 +1,13 @@
-Swing_Module
+import javax.swing.*;
+
+public class Swing_Module {
+    public static void main(String[] args) {
+        
+        JFrame fm = new JFrame();
+        fm.setVisible(true); // set Frame Visibility.
+        fm.setSize(420,420); // set Frame Size.
+        fm.setTitle("RAUNAK GUPTA"); // set Frame Title.
+        fm.setResizable(false); // Disabling the frame resizable option
+        fm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set Frame close option
+    }
+}
