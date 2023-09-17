04fc18e372e07309c60923316f6edafec3161181
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-09-17 20:11:29 +0530

    Update Swing_Module.java

--- Mini Projects/Java/Swing_Module.java
+++ Mini Projects/Java/Swing_Module.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.Color;
 
 public class Swing_Module {
     public static void main(String[] args) {
@@ -9,5 +10,14 @@ public class Swing_Module {
         fm.setTitle("RAUNAK GUPTA"); // set Frame Title.
         fm.setResizable(false); // Disabling the frame resizable option
         fm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set Frame close option
-    }
+
+
+        ImageIcon img = new ImageIcon("logo.png"); // Creating ImageIcon class object to set image.
+        fm.setIconImage(img.getImage()); // Setting up the image using setIconImage() & getImage() functions
+        fm.getContentPane().setBackground(new Color(204, 197, 185)); // setting up the color.
+
+
+
+
+        }
 }
