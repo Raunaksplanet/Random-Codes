b1427d9e4aee2661f8d5bca81779b671ce3d9d59
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-09-17 20:18:11 +0530

    Update Swing_Module.java

--- Mini Projects/Java/Swing_Module.java
+++ Mini Projects/Java/Swing_Module.java
@@ -4,16 +4,19 @@ import java.awt.Color;
 public class Swing_Module {
     public static void main(String[] args) {
         
+        // 1.Basic of JFrame class
         JFrame fm = new JFrame();
+        
         fm.setVisible(true); // set Frame Visibility.
         fm.setSize(420,420); // set Frame Size.
         fm.setTitle("RAUNAK GUPTA"); // set Frame Title.
-        fm.setResizable(false); // Disabling the frame resizable option
-        fm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set Frame close option
 
+        fm.setResizable(false); // Disabling the frame resizable option.
+        fm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set Frame close option.
 
         ImageIcon img = new ImageIcon("logo.png"); // Creating ImageIcon class object to set image.
-        fm.setIconImage(img.getImage()); // Setting up the image using setIconImage() & getImage() functions
+        fm.setIconImage(img.getImage()); // Setting up the image using setIconImage() & getImage() functions.
+        
         fm.getContentPane().setBackground(new Color(204, 197, 185)); // setting up the color.
 
 
