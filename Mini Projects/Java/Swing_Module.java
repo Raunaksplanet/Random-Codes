b9980a204099a11a2a6dfd10cf4fcf73477a768c
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-09-18 19:12:15 +0530

    Update Swing_Module.java

--- Mini Projects/Java/Swing_Module.java
+++ Mini Projects/Java/Swing_Module.java
@@ -1,61 +1,92 @@
 import javax.swing.*;
-import java.awt.Color;
+import java.awt.*;
 
-public class Swing_Module {
-    public static void main(String[] args) {
+// Create a class named Example2 that extends the JFrame class
+public class Example2 extends JFrame {
+
+    // Constructor for the Example2 class
+    Example2() {
+        // Set up the basic properties of the JFrame
         
-        // 1.Basic of JFrame class
-        JFrame fm = new JFrame();
+        // Make the JFrame visible
+        setVisible(true);
         
-        fm.setVisible(true); // set Frame Visibility.
-        fm.setSize(420,420); // set Frame Size.
-        fm.setTitle("RAUNAK GUPTA"); // set Frame Title.
-
-        fm.setResizable(false); // Disabling the frame resizable option.
-        fm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set Frame close option.
-
-        ImageIcon img = new ImageIcon("logo.png"); // Creating ImageIcon class object to set image.
-        fm.setIconImage(img.getImage()); // Setting up the image using setIconImage() & getImage() functions.
-
-        fm.getContentPane().setBackground(new Color(204, 197, 185)); // setting up the color.
-
-
-        // 2.Basic of JLabel class.
-        // 2.1.Adding Text in GUI Application.
+        // Set the size of the JFrame to 420x420 pixels
+        setSize(420, 420);
         
-        // ImageIcon img2 = new ImageIcon("logo.png"); // Creating ImageIcon class object to set image.
+        // Set the title of the JFrame to "Hello"
+        setTitle("Hello");
+        
+        // Define what happens when the JFrame is closed
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Make the JFrame non-resizable
+        setResizable(false);
+
+        // Create an ImageIcon from a file named "logo.png"
+        ImageIcon img = new ImageIcon("logo.png");
+
+        // Set the icon image of the JFrame
+        setIconImage(img.getImage());
+
+        // Create a JLabel named fl
         JLabel fl = new JLabel();
+        
+        // Create another JLabel named fl2
         JLabel fl2 = new JLabel();
+        
+        // Set the text for fl to "Hello there"
         fl.setText("Hello there");
+        
+        // Set the text for fl2 to "Honey"
         fl2.setText("Honey");
+        
+        // Add fl to the JFrame
         add(fl);
+        
+        // Set the icon for fl to the previously created ImageIcon
         fl.setIcon(img);
+        
+        // Revalidate the JFrame to apply changes
         revalidate();
+        
+        // Add fl2 to the JFrame
         add(fl2);
 
-
+        // Set the horizontal text position of fl2 to the center
         fl2.setHorizontalTextPosition(JLabel.CENTER);
+        
+        // Set the vertical text position of fl2 to the top
         fl2.setVerticalTextPosition(JLabel.TOP);
         
+        // Set the vertical alignment of fl2 to the center
         fl2.setVerticalAlignment(JLabel.CENTER);
+        
+        // Set the horizontal alignment of fl2 to the center
         fl2.setHorizontalAlignment(JLabel.CENTER);
-        
-        
-        // fl2.setHorizontalAlignment(JLabel.LEFT);
 
-
+        // Set the horizontal text position of fl to the center
         fl.setHorizontalTextPosition(JLabel.CENTER);
+        
+        // Set the vertical text position of fl to the top
         fl.setVerticalTextPosition(JLabel.TOP);
         
+        // Set the vertical alignment of fl to the top
         fl.setVerticalAlignment(JLabel.TOP);
+        
+        // Set the horizontal alignment of fl to the center
         fl.setHorizontalAlignment(JLabel.CENTER);
         
+        // Set the font of fl to "Segoe UI", bold, and size 20
+        fl.setFont(new Font("Segoe UI", Font.BOLD, 20));
         
-        fl.setFont(new Font("Segoe UI",Font.BOLD,20));
-        fl2.setFont(new Font("Segoe UI",Font.ITALIC,30));
+        // Set the font of fl2 to "Segoe UI", italic, and size 30
+        fl2.setFont(new Font("Segoe UI", Font.ITALIC, 30));
+    }
 
-
-
-
-        }
+    // Main method to create an instance of Example2
+    public static void main(String[] args) {
+        // Create an instance of Example2 (the GUI)
+        Example2 gui = new Example2();
+    }
 }
