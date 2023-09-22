0edf4b64fc85e40951c3b2916f8ffe1147eeba38
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-09-22 19:37:16 +0530

    Update

--- Mini Projects/Java/SignIn SignUp System/demo.java
+++ Mini Projects/Java/SignIn SignUp System/demo.java
@@ -1,23 +1,7 @@
-import java.util.Random;
-import java.io.File;
-import java.io.Writer;
+
 
 public class demo {
     public static void main(String[] args) {
-        Random rd = new Random();
-        int n = rd.nextInt(100);
-
-        File fol = new File("User Data");
-        fol.mkdir();
-
-        File fl = new File("User Data\\User");
-        try{
-            fl.createNewFile();
-        }catch(Exception e){
-            e.toString();
-        }
-
         
-
     }
 }
